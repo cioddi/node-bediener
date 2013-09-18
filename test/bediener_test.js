15ad00d645f5994eac0950c078c610a47af0ae02
cioddi <maxtobiasweber@gmail.com>
2013-09-18 18:57:47 +0300

    travis.ci test added

--- test/bediener_test.js
+++ test/bediener_test.js
@@ -8,13 +8,15 @@ var spawn = require('child_process').spawn;
 
 exports['bediener'] = {
   setUp: function(done) {
-    
+
     done();
   },
   'no args': function(test) {
     test.expect(1);
       exec('curl http://localhost:6000/test/testtext.txt',function(err,stdout,stderr){
-
+        console.log(stderr);
+        console.log(err);
+        console.log(stdout);
         test.equal(stdout, 'text123', 'should be "text123".');
         // bediener.close();
         test.done();
